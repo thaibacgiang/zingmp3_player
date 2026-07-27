37f77667f57e8930b61bc72207a35fe83f0fd89c
thaibacgiang <89860738+thaibacgiang@users.noreply.github.com>
2026-07-27 16:07:21 +0700

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,9 @@
+export const CARD_NAME = "zingmp3-media-card";
 
-export const CARD_NAME="zingmp3-media-card";
+export const CARD_VERSION = "2.0.0";
 
-export const CARD_VERSION="2.0.0";
+export const DEFAULT_COVER =
+
+"https://brands.home-assistant.io/_/media_player/logo.png";
+
+export const SEARCH_DELAY = 400;
