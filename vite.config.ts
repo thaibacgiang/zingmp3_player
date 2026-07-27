d8b01ffdcb38f661adc3dce9802c0df50b9ee7df
thaibacgiang <89860738+thaibacgiang@users.noreply.github.com>
2026-07-27 15:52:09 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,14 @@
 
+import {defineConfig} from "vite";
+
+export default defineConfig({
+    build:{
+        lib:{
+            entry:"src/card.ts",
+            formats:["es"],
+            fileName:()=>"zingmp3-media-card.js"
+        },
+        target:"es2022",
+        minify:true
+    }
+});
