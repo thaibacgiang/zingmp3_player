1bc9ef4765fa40f553078fc50515806a3909b4e9
thaibacgiang <89860738+thaibacgiang@users.noreply.github.com>
2026-07-27 15:53:39 +0700

    Update helpers.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,1 +1,22 @@
 
+export const callPlayerService=(
+
+    hass:any,
+
+    service:string,
+
+    data:any
+
+)=>{
+
+    return hass.callService(
+
+        "zingmp3_player",
+
+        service,
+
+        data
+
+    );
+
+};
